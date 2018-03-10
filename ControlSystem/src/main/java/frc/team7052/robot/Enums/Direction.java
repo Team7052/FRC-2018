3948ea22751974cf5a73c23f03e06c5832b3c3db
= <kevin.bai@tbcschools.ca>
2018-03-10 17:46:31 -0500

    deleted old filebase

--- ControlSystem/src/main/java/frc/team7052/robot/Enums/Direction.java
+++
@@ -1,5 +0,0 @@
-package frc.team7052.robot.Enums;
-
-public enum Direction {
-    up, down, left, right, in, out, north, east, south, west, stationary
-}
--- ControlSystem/src/main/java/frc/team7052/robot/Enums/DrivingState.java
+++
@@ -1,5 +0,0 @@
-package frc.team7052.robot.Enums;
-
-public enum DrivingState {
-    careful, regular, turbo
-}
